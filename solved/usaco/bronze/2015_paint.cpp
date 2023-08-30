898eb9b10d7027dcdf4ec7fa509030a9113c8e84
Safa Andaç <8544658+SanD94@users.noreply.github.com>
2023-08-30 13:10:01 +0200

    initial commmit

---
+++ solved/usaco/bronze/2015_paint.cpp
@@ -0,0 +1,31 @@
+// http://www.usaco.org/index.php?page=viewproblem2&cpid=567
+#include <cmath>
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+typedef unsigned long long int ulli;
+
+int main() {
+  freopen("paint.in", "r", stdin);
+  freopen("paint.out", "w", stdout);
+  
+  int f_fi, f_se, b_fi, b_se;
+  int res;
+  cin >> f_fi >> f_se >> b_fi >> b_se;
+  // partial
+  if (f_fi <= b_fi && b_fi <= f_se && f_se <= b_se)
+    res = b_se - f_fi;
+  if (b_fi <= f_fi && f_fi <= b_se && b_se <= f_se)
+    res = f_se - b_fi;
+  // within
+  if (f_fi <= b_fi && b_se <= f_se)
+    res = f_se - f_fi;
+  if (b_fi <= f_fi && f_se <= b_se)
+    res = b_se - b_fi;
+  // seperate
+  if (f_se <= b_fi || b_se <= f_fi)
+    res = f_se - f_fi + b_se - b_fi;
+  cout << res << endl;
+  return 0;
+}
